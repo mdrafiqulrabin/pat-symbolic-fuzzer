1a7c238e65bbee072471b09eb14a7ebc10e691cf
Sonia Atefi <soode.atefi@gmail.com>
2021-04-26 10:11:47 -0500

    gcd

--- examples/gcd.py
+++ examples/gcd.py
@@ -1,17 +1,11 @@
-def check_triangle(a: int, b: int, c: int) -> int:
-    if a == b:
-        if a == c:
-            if b == c:
-                return "Equilateral"
-            else:
-                return "Isosceles"
-        else:
-            return "Isosceles"
-    else:
-        if b != c:
-            if a == c:
-                return "Isosceles"
-            else:
-                return "Scalene"
-        else:
-            return "Isosceles"
+def gcd(a: int, b: int) -> int:
+    if a < b:
+        c: int = a
+        a = b
+        b = c
+
+    while b != 0:
+        c: int = a
+        a = b
+        b = c % b
+    return a
